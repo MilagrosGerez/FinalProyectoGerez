7ed0558668baa80e59569839c14da06c92e3e86f
Milagros Gerez <gerezmilagros03@gmail.com>
2024-09-04 02:00:45 -0300

    librerias

--- js/main.js
+++ js/main.js
@@ -58,6 +58,22 @@ if(productosEnCarritoLS){
 }
 
 function agregarAlCarrito(e){
+
+    Toastify({
+        text: "Has elegido un producto!",
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id ===idBoton);
     
