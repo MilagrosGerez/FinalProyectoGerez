f21d0b4ca47f7297e720d1a26a329a71f408a5f9
Milagros Gerez <gerezmilagros03@gmail.com>
2024-09-03 22:14:57 -0300

    arreglando JS

--- js/main.js
+++ js/main.js
@@ -37,7 +37,9 @@ const productos = [
     }
 ];
 const contenedorProductos = document.querySelector("#contenedor-productos")
-
+const tituloPrincipal = document.querySelector('#titulo-principal');
+let botonesAgregar = document.querySelectorAll('.producto-agregar');
+const numerito = document.querySelector('#numero');
 function cargarProductos(){
     productos.forEach(producto =>{
         
@@ -56,6 +58,41 @@ function cargarProductos(){
         
 
     })
+
+    actualizarBotonesAgregar();
 };
 
-cargarProductos();
+cargarProductos(productos);
+
+function actualizarBotonesAgregar(){
+    botonesAgregar = document.querySelectorAll('.producto-agregar');
+
+    botonesAgregar.forEach(boton =>{
+        boton.addEventListener('click', agregarAlCarrito);
+    }  )
+}
+
+const productosEnCarrito = [];
+function agregarAlCarrito(e){
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id ===idBoton);
+    
+
+    if(productosEnCarrito.some(producto=> producto.id === idBoton))  {
+        const index = productosEnCarrito.findIndex(producto => producto.id ===idBoton);
+        productosEnCarrito[index].cantidad++;
+    }else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+
+    }
+    actualizarNumerito();
+    
+    localStorage.setItem('productos-en-carrito' , JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito (){
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
